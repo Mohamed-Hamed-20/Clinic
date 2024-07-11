6c7d79eb5eae7eec5331c68dfb5278a99d4dab84
mohamed hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-07-12 00:29:14 +0300

    Update errorHandling.js

--- services/errorHandling.js
+++ services/errorHandling.js
@@ -1,13 +1,20 @@
 export const asyncHandler = (controller) => {
   return (req, res, next) => {
     controller(req, res, next).catch(async (error) => {
+      const validStatusCode =
+        error.code &&
+        Number.isInteger(error.code) &&
+        error.code >= 100 &&
+        error.code < 600;
+      const statusCode = validStatusCode ? error.code : 500;
+
       let result = {};
 
       process.env.MOOD == "DEV"
         ? (result = { message: error.message, stack: error.stack })
         : (result = { message: "something went wrong ! , SERVER ERROR ! :( " });
 
-      return res.status(error.code || 500).json(result);
+      return res.status(statusCode || 500).json(result);
     });
   };
 };
