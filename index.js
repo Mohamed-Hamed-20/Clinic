cca6037067ee50658fb2742f2bfc3a87c87529b6
mohamed hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-07-12 00:19:15 +0300

    add cors orgin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import doctorRouter from "./routes/doctor.routes.js";
 import connectDB from "./DB/connect.js";
@@ -7,6 +8,26 @@ import { GlobalErrorHandling } from "./services/errorHandling.js";
 const app = express();
 
 dotenv.config({ path: "./.env" });
+
+const allowedOrigins = [
+  "https://graduation-project-beryl-seven.vercel.app/",
+  "https://graduation-project-beryl-seven.vercel.app",
+  "http://localhost:3000/",
+  "http://localhost:3000",
+  "https://localhost:3000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // parse Data
 app.use(express.json({ limit: "100kb" }));
